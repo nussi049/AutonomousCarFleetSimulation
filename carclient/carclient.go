711c5d458b9bb2b57aa457a2677fa76e953da388
Emre Sali <117669108+emresali@users.noreply.github.com>
2024-05-21 11:35:23 +0200

    fixxed randomDrive() and limiting carclient to GridCoordinates

--- carclient/carclient.go
+++ carclient/carclient.go
@@ -18,12 +18,15 @@ func init() {
 }
 
 type Client struct {
-	Car    utils.CarInfo
-	Conn   *grpc.ClientConn
-	Client api.CoordinatorServiceClient
+	Car         utils.CarInfo
+	Conn        *grpc.ClientConn
+	Client      api.CoordinatorServiceClient
+	GridWidth   int
+	GridHeight  int
+	LastMoveDir int // 0: up, 1: down, 2: left, 3: right
 }
 
-func newCarClient(identifier string, route []utils.Coordinate) *Client {
+func newCarClient(identifier string, startPos utils.Coordinate, gridWidth, gridHeight int) *Client {
 	// Establish a connection to the coordinator service via gRPC
 	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
@@ -32,54 +35,96 @@ func newCarClient(identifier string, route []utils.Coordinate) *Client {
 	}
 
 	client := api.NewCoordinatorServiceClient(conn)
-	var startPos utils.Coordinate
-	if len(route) > 0 {
-		startPos = route[0]
-	}
 
 	return &Client{
 		Car: utils.CarInfo{
 			Identifier:  identifier,
 			Position:    startPos,
-			Route:       route,
-			ActiveRoute: false,
+			Route:       []utils.Coordinate{}, // Empty route to start with
+			ActiveRoute: true,
 		},
-		Conn:   conn,
-		Client: client,
+		Conn:        conn,
+		Client:      client,
+		GridWidth:   gridWidth,
+		GridHeight:  gridHeight,
+		LastMoveDir: -1, // Initialize to an invalid direction
 	}
 }
 
 func (c *Client) drive() {
-	if len(c.Car.Route) > 1 {
-		c.Car.Route = c.Car.Route[1:]   // Move to the next position in the route
-		c.Car.Position = c.Car.Route[0] // Update the current position
+	if len(c.Car.Route) > 0 {
+		c.Car.Position = c.Car.Route[0] // Update the current position to the first element
+		c.Car.Route = c.Car.Route[1:]   // Remove the first element from the route
 	} else {
-		// No route defined, perform a random drive
+		// No route defined or route completed, perform a random drive
 		c.randomDrive()
 	}
 	fmt.Printf("Driving to new position: X: %d, Y: %d\n", c.Car.Position.X, c.Car.Position.Y)
 	c.sendPosition() // Send updated position to the coordinator
 }
 
-// randomDrive performs a random movement forward or sideways
 func (c *Client) randomDrive() {
-	// Choose randomly between moving horizontally or vertically
-	moveVertically := rand.Intn(2) == 0 // Randomly choose between true (vertical) or false (horizontal)
+	var moveDirection int
 
-	if moveVertically {
-		// Move vertically up or down
-		deltaY := rand.Intn(2) // Randomly 0 or 1
-		if deltaY == 0 {
-			deltaY = -1 // Move down
+	for {
+		moveDirection = rand.Intn(4) // Randomly choose between 0 (up), 1 (down), 2 (left), 3 (right)
+
+		if moveDirection == c.oppositeDirection() {
+			continue // Skip if it is the opposite of the last move
 		}
-		c.Car.Position.Y += int32(deltaY)
-	} else {
-		// Move horizontally left or right
-		deltaX := rand.Intn(2) // Randomly 0 or 1
-		if deltaX == 0 {
-			deltaX = -1 // Move left
+
+		newPosition := c.Car.Position
+
+		switch moveDirection {
+		case 0:
+			// Move vertically up
+			if newPosition.Y > 0 {
+				newPosition.Y -= 1
+			} else {
+				continue // Skip if the move is out of bounds
+			}
+		case 1:
+			// Move vertically down
+			if newPosition.Y < int32(c.GridHeight-1) {
+				newPosition.Y += 1
+			} else {
+				continue // Skip if the move is out of bounds
+			}
+		case 2:
+			// Move horizontally left
+			if newPosition.X > 0 {
+				newPosition.X -= 1
+			} else {
+				continue // Skip if the move is out of bounds
+			}
+		case 3:
+			// Move horizontally right
+			if newPosition.X < int32(c.GridWidth-1) {
+				newPosition.X += 1
+			} else {
+				continue // Skip if the move is out of bounds
+			}
 		}
-		c.Car.Position.X += int32(deltaX)
+
+		// If the new position is valid and not reversing the last move, update the position and break the loop
+		c.LastMoveDir = moveDirection
+		c.Car.Position = newPosition
+		break
+	}
+}
+
+func (c *Client) oppositeDirection() int {
+	switch c.LastMoveDir {
+	case 0:
+		return 1 // Opposite of up is down
+	case 1:
+		return 0 // Opposite of down is up
+	case 2:
+		return 3 // Opposite of left is right
+	case 3:
+		return 2 // Opposite of right is left
+	default:
+		return -1 // No valid last direction
 	}
 }
 
@@ -119,8 +164,10 @@ func (c *Client) periodicPositionUpdate() {
 }
 
 func StartClient() {
-	route := []utils.Coordinate{{X: 0, Y: 0}, {X: 1, Y: 1}, {X: 2, Y: 2}}
-	car := newCarClient("TestCar1", route)
+	startPos := utils.Coordinate{X: 3, Y: 3}
+	gridWidth := 8
+	gridHeight := 8
+	car := newCarClient("TestCar1", startPos, gridWidth, gridHeight)
 	if car == nil {
 		fmt.Println("Failed to create car client")
 		return
